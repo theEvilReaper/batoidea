4c9624a209de219f28effbd4b2594fc913af6610
theEvilReaper <steffenwx@gmail.com>
2021-07-16 17:15:22 +0200

    Add implementation for the channel interaction

--- src/main/java/net/theEvilReaper/batoidea/interaction/InteractionFactory.java
+++ src/main/java/net/theEvilReaper/batoidea/interaction/InteractionFactory.java
@@ -7,6 +7,9 @@ import net.theEvilReaper.bot.api.interaction.InteractionType;
 import org.jetbrains.annotations.NotNull;
 
 /**
+ * The {@link InteractionFactory} is the implementation of the {@link AbstractInteractionFactory}.
+ * The factory handles the instance of each interaction class from the project. It also reduce the instances from the
+ * given interactions to one per typ. The factory provides a method to get the right {@link Interaction} for a use case.
  * @author theEvilReaper
  * @version 1.0.0
  * @since 1.0.0
@@ -16,22 +19,35 @@ public class InteractionFactory extends AbstractInteractionFactory {
 
     private final Interaction[] interactions;
 
+    /**
+     * Creates all instances for the given implementations for the {@link Interaction}.
+     * @param socket The socket from the {@link LocalTeamspeakClientSocket}
+     */
+
     public InteractionFactory(LocalTeamspeakClientSocket socket) {
         this.interactions = new Interaction[InteractionType.getValues().length];
         this.interactions[0] = new ClientInteraction(socket);
         this.interactions[1] = new GroupInteraction(socket);
         this.interactions[2] = new ServerInteraction(socket);
+        this.interactions[3] = new ServerChannelInteraction(socket);
     }
+
+    /**
+     * Get a specific implementation of the {@link Interaction} from the factory.
+     * Note that a wrong clazzType throws an {@link IllegalArgumentException}
+     * @param type The type which implementation should be determine
+     * @param clazzType The implementation class from the {@link Interaction}
+     * @param <T> The value must extends from the {@link Interaction}. Other types are not allowed
+     * @return The determined {@link Interaction} implementation.
+     */
 
     @Override
     public <T extends Interaction> T getInteraction(@NotNull InteractionType type, Class<T> clazzType) {
         var interaction = interactions[type.getId()];
-        //TODO: FIx that
-        /*
-        if (!interaction.getClass().getSimpleName().equals(clazzType.getSimpleName())) {
+
+        /*if (!interaction.getClass().isInstance(clazzType)) {
             throw new IllegalArgumentException("The given type and the clazz are not similar");
         }*/
-
         return clazzType.cast(interaction);
     }
 }
---
+++ src/main/java/net/theEvilReaper/batoidea/interaction/ServerChannelInteraction.java
@@ -0,0 +1,73 @@
+package net.theEvilReaper.batoidea.interaction;
+
+import com.github.manevolent.ts3j.api.Permission;
+import com.github.manevolent.ts3j.command.CommandException;
+import com.github.manevolent.ts3j.protocol.socket.client.LocalTeamspeakClientSocket;
+import net.theEvilReaper.bot.api.interaction.ChannelInteraction;
+import net.theEvilReaper.bot.api.util.Conditions;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author theEvilReaper
+ * @version 1.0.0
+ * @since 1.0.0
+ **/
+
+public class ServerChannelInteraction implements ChannelInteraction {
+
+    private final LocalTeamspeakClientSocket socket;
+
+    public ServerChannelInteraction(LocalTeamspeakClientSocket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void sendChannelMessage(int channelID, @NotNull String message) {
+        Conditions.checkForEmpty(message);
+        try {
+            socket.sendChannelMessage(channelID, message);
+        } catch (IOException | CommandException | InterruptedException | TimeoutException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    @Override
+    public void joinChannel(int channelID, @Nullable String password) {
+        try {
+            socket.joinChannel(channelID, password);
+        } catch (IOException | CommandException | InterruptedException | TimeoutException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    @Override
+    public void addChannelPermission(int channelID, @Nullable Permission... permissions) {
+        try {
+            socket.channelAddPermission(channelID, permissions);
+        } catch (IOException | CommandException | InterruptedException | TimeoutException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    @Override
+    public void addChannelClientPermission(int channelID, int clientDatabaseID, @Nullable Permission... permissions) {
+        try {
+            socket.channelClientAddPermission(channelID, clientDatabaseID, permissions);
+        } catch (IOException | CommandException | InterruptedException | TimeoutException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    @Override
+    public void removeChannelClientPermission(int channelID, int clientDatabaseID, @Nullable Permission... permissions) {
+        try {
+            socket.channelClientDeletePermission(channelID, clientDatabaseID, permissions);
+        } catch (IOException | CommandException | InterruptedException | TimeoutException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
