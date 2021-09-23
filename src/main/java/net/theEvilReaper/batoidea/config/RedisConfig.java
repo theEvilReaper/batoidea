64633c905c99607415ac12de0e79614388d970e7
theEvilReaper <steffenwx@gmail.com>
2021-09-23 11:52:00 +0200

    Remove some code

--- src/main/java/net/theEvilReaper/batoidea/config/RedisConfig.java
+++ src/main/java/net/theEvilReaper/batoidea/config/RedisConfig.java
@@ -2,16 +2,10 @@ package net.theEvilReaper.batoidea.config;
 
 import net.theEvilReaper.bot.api.config.Config;
 
-
 public class RedisConfig extends Config {
-
-  //  private final Path filePath = Paths.get(RESOURCE_PATH, "rabbit.properties");
 
     @Override
     public void load() {
-     //   if (Files.exists(filePath)) {
-
-      //  }
     }
 
     @Override
