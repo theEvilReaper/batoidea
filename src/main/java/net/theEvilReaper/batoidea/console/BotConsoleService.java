bca8ba0b9c8738ee0b0b2896be0b4e3b0743fb81
theEvilReaper <steffenwx@gmail.com>
2021-10-24 16:48:59 +0200

    Update the service to new command structure

--- src/main/java/net/theEvilReaper/batoidea/console/BotConsoleService.java
+++ src/main/java/net/theEvilReaper/batoidea/console/BotConsoleService.java
@@ -1,21 +1,15 @@
 package net.theEvilReaper.batoidea.console;
 
 import net.theEvilReaper.batoidea.Batoidea;
-import net.theEvilReaper.batoidea.command.console.ExitCommand;
-import net.theEvilReaper.batoidea.command.console.HelpCommand;
-import net.theEvilReaper.batoidea.command.console.NotifyCommand;
-import net.theEvilReaper.batoidea.command.console.UserCommand;
-import net.theEvilReaper.bot.api.command.Command;
-import net.theEvilReaper.bot.api.command.ConsoleCommand;
+import net.theEvilReaper.bot.api.command.CommandManager;
+import net.theEvilReaper.bot.api.command.CommandSender;
+import net.theEvilReaper.bot.api.command.ConsoleSender;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 import java.util.logging.Level;
-import java.util.logging.Logger;
 
+import static net.theEvilReaper.bot.api.util.Strings.*;
 
 /**
  * @author theEvilReaper
@@ -26,38 +20,25 @@ import java.util.logging.Logger;
 @Deprecated(forRemoval = true)
 public class BotConsoleService {
 
-    private static final String[] EMPTY_ARRAY = new String[0];
+    private static final CommandSender CONSOLE = new ConsoleSender();
 
-    private final Map<String, Command> commandMap;
-    private final Logger logger;
-    private final BotConsole console;
+    private final CommandManager commandManager;
 
     private final Scanner scanner;
     private final Thread thread;
     private boolean exit;
 
-    public BotConsoleService(Logger logger, Batoidea batoidea) {
-        this.logger = logger;
+    public BotConsoleService(Batoidea batoidea, CommandManager commandManager) {
         this.exit = batoidea.isStopping();
-        this.commandMap = new HashMap<>();
-        this.console = new BotConsole(logger);
+        this.commandManager = commandManager;
         this.scanner = new Scanner(System.in);
         this.thread = this.init();
         this.thread.setDaemon(true);
         this.thread.setUncaughtExceptionHandler((thread1, throwable) -> {
-            logger.log(Level.WARNING, throwable.getMessage());
+            batoidea.getLogger().log(Level.WARNING, throwable.getMessage());
         });
         this.thread.setName("Console-Thread");
         this.thread.start();
-        this.initCommands(batoidea);
-    }
-
-    private void initCommands(Batoidea batoidea) {
-        this.logger.log(Level.INFO, "Registering commands");
-        this.registerCommand(new ExitCommand(batoidea));
-        this.registerCommand(new HelpCommand());
-        this.registerCommand(new NotifyCommand(batoidea.getSupportService()));
-        this.registerCommand(new UserCommand(batoidea.getUserService(), batoidea.getClientProvider()));
     }
 
     private Thread init() {
@@ -65,63 +46,23 @@ public class BotConsoleService {
             while (!exit) {
                 String input = this.scanner.nextLine();
                 if (input == null || input.isEmpty()) return;
-
                 handleInput(input);
             }
         });
     }
 
     private void handleInput(@NotNull String line) {
-        var split = Batoidea.SPLIT_PATTERN.split(line);
+        var split = SPLIT_PATTERN.split(line);
 
         if (split.length == 0) {
             return;
         }
 
-        String cmd = split[0];
-        String[] args;
+        String command = split[0];
+        String[] arguments;
 
-        args = split.length == 1 ? EMPTY_ARRAY : new String[split.length - 1];
-        System.arraycopy(split, 1, args, 0, split.length - 1);
-        dispatch(cmd, args);
-    }
-
-    /**
-     * Register a new {@link ConsoleCommand} to the known commands.
-     * @param command The class from the command that inherit from the {@link ConsoleCommand}
-     */
-
-    public void registerCommand(@NotNull Command command) {
-        commandMap.put(command.getName().toLowerCase(), command);
-    }
-
-    /**
-     * Unregister a command from the console commands.
-     * @param name The name of the command to remove
-     */
-
-    public void unregisterCommand(@NotNull String name) {
-        commandMap.remove(name);
-    }
-
-    /**
-     * Executes a {@link ConsoleCommand}.
-     * @param cmd The name of the command to dispatch
-     * @param args The arguments from the command
-     */
-
-    public void dispatch(@NotNull String cmd, @Nullable String... args) {
-        var command = commandMap.get(cmd.toLowerCase());
-
-        if (command == null) {
-           logger.log(Level.INFO, "Unknown command. Type help for help");
-            return;
-        }
-
-        try {
-           // command.execute(console, cmd, args);
-        } catch (Exception exception) {
-            logger.log(Level.SEVERE,"Error while executing command: " + cmd, exception);
-        }
+        arguments = split.length == 1 ? EMPTY_ARRAY : new String[split.length - 1];
+        System.arraycopy(split, 1, arguments, 0, split.length - 1);
+        commandManager.executeCommand(CONSOLE, command, arguments);
     }
 }
