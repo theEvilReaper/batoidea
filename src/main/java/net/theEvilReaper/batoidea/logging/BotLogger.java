9b5ef6db70a5fde1aaeb6b7e043a79943ab65882
theEvilReaper <steffenwx@gmail.com>
2022-10-11 11:50:05 +0200

    Remove unusedd code lines

--- src/main/java/net/theEvilReaper/batoidea/logging/BotLogger.java
+++ src/main/java/net/theEvilReaper/batoidea/logging/BotLogger.java
@@ -17,16 +17,11 @@ import java.util.logging.SimpleFormatter;
  * @version 1.0.0
  * @since 1.0.0
  **/
-
 public class BotLogger extends Logger {
 
     public BotLogger() {
         super("BotLogger", null);
-
         setLevel(Level.ALL);
-
-        // setUseParentHandlers(true);
-
         Path logsFolder = Paths.get("logs");
         try {
             Files.createDirectories(logsFolder);
@@ -47,8 +42,8 @@ public class BotLogger extends Logger {
     }
 
     @Override
-    public void log(LogRecord record) {
-        record.setMessage(record.getMessage());
-        super.log(record);
+    public void log(LogRecord aRecord) {
+        aRecord.setMessage(aRecord.getMessage());
+        super.log(aRecord);
     }
 }
